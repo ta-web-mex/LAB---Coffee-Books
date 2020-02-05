68ef47731d651103c67eb58a5d67c439751dabb2
America Zurita <americazurita@gmail.com>
2020-02-05 09:22:27 -0600

    done

--- coffee-books/controllers/placesControllers.js
+++ coffee-books/controllers/placesControllers.js
@@ -29,6 +29,34 @@ exports.placesView=async (req,res)=>{
   res.render("folder/places",{places})
 }
 
+exports.detailPlace=async(req,res)=>{
+  const {id}=req.params;
+  const place=await Places.findById(id)
+  res.render("folder/detailed",place);
+}
+
+
+exports.detailPlacePost=async (req,res,next)=>{
+ await console.log(req.params.id)
+  const { name, address, latitude, longitude, placeType } = req.body
+  const updatePlace = {
+    name,
+    location: {
+      address,
+      coordinates: [longitude, latitude]
+    },
+    placeType
+  }
+  await Places.findByIdAndUpdate(req.params.id, updatePlace);
+
+  res.redirect("/places");
+}
+
+exports.deletePlace= async(req,res,next)=>{
+await Places.findByIdAndDelete(req.params.id);
+res.redirect("/places")
+}
+
 /*
 // R in CRUD
 exports.getPlaces = async(req, res) =>{
--- coffee-books/routes/index.js
+++ coffee-books/routes/index.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 const passport = require("../config/passport")
 
 const {signup, signUpView, login, loginView, logout} = require("../controllers/authControllers")
-const {createPlaceView,placesView,placePost}=require("../controllers/placesControllers")
+const {createPlaceView,placesView,placePost,detailPlace,detailPlacePost,deletePlace}=require("../controllers/placesControllers")
 const {isAuthenticated,checkRole}=require("../middlewares")
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -15,9 +15,11 @@ router.post('/signup',signup)
 router.get("/login", loginView)
 router.post("/login",
             passport.authenticate("local",{
-              successRedirect:"/create",
+              successRedirect:"/places",
               failureRedirect:"/login",
-              failureFlash:true
+              failureFlash:true,
+              failureMessage:"The input data is incorrect"
+              
             }))
 
             //Aqui va facebook
@@ -37,5 +39,8 @@ router.get('/create',isAuthenticated,createPlaceView)
   //router.get("/create",placesView)
   router.post("/create",placePost)
   router.get("/places",placesView)
+  router.get('/places/:id', isAuthenticated, detailPlace)
+  router.post('/places/:id', isAuthenticated, detailPlacePost)
+  router.get('/delete/:id', isAuthenticated, deletePlace)
 
 module.exports = router;
