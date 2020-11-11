303c7667259ae901e006d1fc9f231adac295003b
Tomi Scattiniclear <tomasscattini@gmail.com>
2020-11-11 09:57:59 -0500

    no mas mensajes en consola

--- coffee-books/app.js
+++ coffee-books/app.js
@@ -12,9 +12,12 @@ const passport = require("./config/passport")
 
 
 mongoose
-    .connect('mongodb://localhost/coffee-books', { useNewUrlParser: true })
+    .connect('mongodb://localhost/coffee-books', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
     .then(x => {
-        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+        // console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     })
     .catch(err => {
         console.error('Error connecting to mongo', err)
